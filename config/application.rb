8fb1dc451eb5b9bd3ead43de333480c46cac3833
Keith Rodger <kprodger@btinternet.com>
2012-02-20 16:09:59 -0800

    Finished layouts and routes

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,13 @@ module SampleApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    # Add this for Spork
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
